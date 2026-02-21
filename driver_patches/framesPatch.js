7509997f13982a5461d04b49d4a7da3766791ded
Vinyzu <50874994+Vinyzu@users.noreply.github.com>
2026-02-21 16:31:52 +0100

    Revert changes to make page suite pass

--- driver_patches/framesPatch.js
+++ driver_patches/framesPatch.js
@@ -49,7 +49,7 @@ export function patchFrames(project) {
     evalOnSelectorMethod.setBodyText(`const handle = await this.selectors.query(selector, { strict }, scope);
         if (!handle)
           throw new Error('Failed to find element matching selector "' + selector + '"');
-        const result = await handle.evaluateExpression(expression, { isFunction }, arg, false);
+        const result = await handle.evaluateExpression(expression, { isFunction }, arg, true);
         handle.dispose();
         return result;`)
 
