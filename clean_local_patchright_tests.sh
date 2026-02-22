2393930245d493df9c5f78e8c342ef2e91b905c7
Vinyzu <50874994+Vinyzu@users.noreply.github.com>
2026-02-22 23:57:00 +0100

    Tests Helper Script

---
+++ clean_local_patchright_tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -rf playwright 2>/dev/null || true
+git clone https://github.com/microsoft/playwright --branch v1.58.2
+cd playwright
+npm ci
+node "../patchright_nodejs_patch.js"
+node utils/generate_channels.js
+npm run build
+npx playwright install-deps
+npx playwright install chromium
+cd ..
+node modify_tests.js
+cd playwright
+PWTEST_MODE=driver npx playwright test --config=tests/library/playwright.config.ts --project=chromium-page --max-failures=0 --reporter=null --workers=16 | tee -a ./test_output.txt
+PWTEST_MODE=driver npx playwright test --config=tests/library/playwright.config.ts --project=chromium-library --max-failures=0 --reporter=null --workers=16 | tee -a ./test_output.txt
