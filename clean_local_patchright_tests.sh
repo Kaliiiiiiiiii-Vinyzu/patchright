cf14f8d6c5ee94d44e3d7d00299617c8985c9c37
Vinyzu <50874994+Vinyzu@users.noreply.github.com>
2026-02-25 07:24:40 +0100

    Not sure about this one

--- clean_local_patchright_tests.sh
+++ clean_local_patchright_tests.sh
@@ -12,5 +12,5 @@ npx playwright install chromium
 cd ..
 node modify_tests.js
 cd playwright
-PWTEST_MODE=driver npx playwright test --config=tests/library/playwright.config.ts --project=chromium-page --max-failures=0 --reporter=null --workers=16 | tee -a ./test_output.txt
-PWTEST_MODE=driver npx playwright test --config=tests/library/playwright.config.ts --project=chromium-library --max-failures=0 --reporter=null --workers=16 | tee -a ./test_output.txt
+PWTEST_MODE=driver npx playwright test --config=tests/library/playwright.config.ts --project=chromium-page --max-failures=0 --reporter=null | tee -a ../test_output.txt
+PWTEST_MODE=driver npx playwright test --config=tests/library/playwright.config.ts --project=chromium-library --max-failures=0 --reporter=null | tee -a ../test_output.txt
