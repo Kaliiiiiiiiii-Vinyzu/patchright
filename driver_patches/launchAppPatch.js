8cb0b5deb66ce8bb018c57e71c14d64d8b8a79c9
Vinyzu <50874994+Vinyzu@users.noreply.github.com>
2026-03-04 23:01:52 +0100

    __pw_saveSerializedSettings -> _saveSerializedSettings

--- driver_patches/launchAppPatch.js
+++ driver_patches/launchAppPatch.js
@@ -9,14 +9,8 @@ export function patchLaunchApp(project) {
   const functionText = syncLocalStorageWithSettings.getText();
   syncLocalStorageWithSettings.replaceWithText(
     functionText.replace(
-      "'_saveSerializedSettings'",
-      "'__pw_saveSerializedSettings'",
-    ).replace(
       "(window as any)._saveSerializedSettings(JSON.stringify({ ...localStorage }));",
-      "(window as any).__pw_saveSerializedSettings(JSON.stringify({ ...localStorage }));",
-    ).replace(
-      "Object.entries(settings).map(([k, v]) => localStorage[k] = v);",
-      "Object.entries(settings).map(([k, v]) => localStorage[k] = v);\n          if (typeof (window as any).__pw_saveSerializedSettings === 'function')\n            (window as any)._saveSerializedSettings = (window as any).__pw_saveSerializedSettings;",
-    ),
+      "if (typeof (window as any)._saveSerializedSettings === 'function')\n            (window as any)._saveSerializedSettings(JSON.stringify({ ...localStorage }));",
+    )
   );
 }
