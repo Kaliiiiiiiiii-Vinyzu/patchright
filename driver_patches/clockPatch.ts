1095d2109552ef9528f1f2cb21597d79eefa3a32
Vinyzu <50874994+Vinyzu@users.noreply.github.com>
2026-04-10 17:38:18 +0200

    Clock InitScript Installment

--- driver_patches/clockPatch.ts
+++ driver_patches/clockPatch.ts
@@ -10,6 +10,25 @@ export function patchClock(project: Project) {
 	// ------- Page Class -------
 	const clockClass = clockSourceFile.getClassOrThrow("Clock");
 
+	// -- _installIfNeeded Method --
+	const installIfNeededMethod = clockClass.getMethodOrThrow("_installIfNeeded");
+	installIfNeededMethod
+		.getBodyOrThrow()
+		.asKindOrThrow(SyntaxKind.Block)
+		.getStatements()[0]
+		.replaceWithText(`
+		if (this._initScripts.length) {
+			const initScriptSources = JSON.stringify(this._initScripts.map((initScript) => initScript.source));
+			await this._evaluateInFrames(\`(() => {
+				if (globalThis.__pwClock?.controller)
+					return;
+				for (const source of \${initScriptSources})
+					eval(source);
+			})();\`);
+			return;
+		}
+	`);
+
 	// -- _evaluateInFrames Method --
 	const evaluateInFramesMethod = clockClass.getMethodOrThrow("_evaluateInFrames");
 	// Modify the constructor's body to include Custom Code
